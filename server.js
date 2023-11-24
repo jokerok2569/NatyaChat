99b19b5467bfaf81bcb3cd9f4d694009c36d5eb6
jokerok2569 <jokerok2569@gmail.com>
2023-11-24 18:38:18 +0200

    3

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const pathToIndex = path.join(__dirname, 'static', 'index.html');
-const indexHtmlFile = fs.readFileSync(pathToIndex);
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
+const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
 
 const server = http.createServer((req, res) => {
 
