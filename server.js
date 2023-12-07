f923f059515e37858fc82c5b166c75e8b223efa8
jokerok2569 <jokerok2569@gmail.com>
2023-12-07 17:22:19 +0200

    1

--- server.js
+++ server.js
@@ -24,4 +24,10 @@ const io = new Server(server);
 
 io.on('connection', (socket) => {
   console.log('a user connected. id - ' + socket.id) ;
+
+  socket.on('new_message', (message) => {
+
+    console.log(message);
+
+  });
 });
