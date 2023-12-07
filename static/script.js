6dd39d2c5a1a970d4e077a78d5d23a4ef65ec42f
jokerok2569 <jokerok2569@gmail.com>
2023-12-07 17:24:23 +0200

    1

--- static/script.js
+++ static/script.js
@@ -1,2 +1,22 @@
-// alert('Hello')
-const socket = io()
+const socket = io();
+
+const messages = document.getElementById('messages');
+
+const form = document.getElementById('form');
+
+const input = document.getElementById('input');
+
+
+form.addEventListener('submit', function(e) {
+
+  e.preventDefault();
+
+  if (input.value) {
+
+    socket.emit('new_message', input.value);
+
+    input.value = '';
+
+  }
+
+});
